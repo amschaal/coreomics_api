a4ce249b7e2e91a6b5f302db18ca4bab8f1895e7
adam <amschaal@gmail.com>
2020-10-23 16:53:59 -0700

    add sharing to bioshare

--- plugins/bioshare.py
+++ plugins/bioshare.py
@@ -3,17 +3,21 @@ from coreomics.requests import CoreomicsAPI
 class SubmissionShare(object):
     def __init__(self, submission_id):
         self.id = submission_id
-        self.share = self.get_share()
+        self.data = self.get_share()
+        self.permissions = None
     @property
     def url(self):
-        return self.share.get('url',None) if self.share else None
+        return self.data.get('url',None) if self.data else None
     def exists(self):
-        return self.share is not None
+        return self.data is not None
     def get_share(self):
         try:
             return CoreomicsAPI().request('/api/bioshare/submission_shares/{}/'.format(self.id))
         except:
             return None
     def create_share(self):
-        if not self.share:
-            self.share = CoreomicsAPI().request('/api/bioshare/submission_shares/', 'POST', {'submission': self.id})
+        if not self.data:
+            self.data = CoreomicsAPI().request('/api/bioshare/submission_shares/', 'POST', {'submission': self.id})
+    def share(self):
+        self.permissions = CoreomicsAPI().request('/api/bioshare/submission_shares/{}/share/'.format(self.id), 'POST', {'submission': self.id})
+        return self.permissions
